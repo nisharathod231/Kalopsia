25e90aefaeea991272cc9f03a55bd8d2d291716f
Nishtha Paul <nishthapaul@Nishthas-MacBook-Air.local>
2024-04-27 23:13:33 +0530

    Add get api for products

--- FrontEnd/src/Pages/ShopCategory.jsx
+++ FrontEnd/src/Pages/ShopCategory.jsx
@@ -1,18 +1,44 @@
-// ShopCategory.jsx
-import React, { useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import './CSS/ShopCategory.css';
-import { ShopContext } from '../Context/ShopContext';
 import Item from '../Components/Item/Item';
+import axios from 'axios';
 
 const ShopCategory = (props) => {
-  const { all_product } = useContext(ShopContext);
-  const categoryProducts = all_product.filter(item => item.category === props.category);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:9010/api/products');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+if (isLoading) {
+    return <p>Loading data...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  // Filter data and display items
+  const filteredItems = data.filter(item => item.category === props.category);
 
   return (
     <div className='shop-category'>
       <div className="shopcategory-products">
-        {categoryProducts.map((item) => (
-          <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+        {filteredItems.map((item) => (
+          <Item key={item.id} id={item.id} name={item.name} image={item.image} new_price={item.newPrice} old_price={item.oldPrice} />
         ))}
       </div>
     </div>
